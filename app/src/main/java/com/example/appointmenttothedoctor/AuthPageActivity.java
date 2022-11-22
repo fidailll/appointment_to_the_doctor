b5806e53fc70584be389b45fa2171d3548d9fdcf
Фидаиль Забиров <fidail2504@outlook.com>
2022-11-22 06:17:02 +0300

    v 0.0.03

---
+++ app/src/main/java/com/example/appointmenttothedoctor/AuthPageActivity.java
@@ -0,0 +1,18 @@
+package com.example.appointmenttothedoctor;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.appointmenttothedoctor.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AuthPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.auth_page);
+
+    }
+}
