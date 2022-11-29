5ed7926c06acb12e0d4e7a41c64afa50da6cbe4b
Фидаиль Забиров <fidail2504@outlook.com>
2022-11-29 06:33:02 +0300

    v 0.0.10

--- app/src/main/java/com/example/appointmenttothedoctor/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/appointmenttothedoctor/ui/profile/ProfileFragment.java
@@ -18,15 +18,19 @@ public class ProfileFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         ProfileViewModel notificationsViewModel =
                 new ViewModelProvider(this).get(ProfileViewModel.class);
 
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView1 = binding.emailAddress;
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView1::setText);
+
+
         return root;
+
 
     }
 
@@ -36,4 +40,27 @@ public class ProfileFragment extends Fragment {
         binding = null;
     }
 
+//    public void getUserProfile() {
+//        // [START get_user_profile]
+//        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//        if (user != null) {
+//            // Name, email address, and profile photo Url
+//            String name = user.getDisplayName();
+//            String email = user.getEmail();
+//            Uri photoUrl = user.getPhotoUrl();
+//
+//            System.out.println(name);
+//            System.out.println(email);
+//            System.out.println(photoUrl);
+//            // Check if user's email is verified
+//            boolean emailVerified = user.isEmailVerified();
+//
+//            System.out.println(emailVerified);
+//            // The user's ID, unique to the Firebase project. Do NOT use this value to
+//            // authenticate with your backend server, if you have one. Use
+//            // FirebaseUser.getIdToken() instead.
+//            String uid = user.getUid();
+//        }
+//        // [END get_user_profile]
+//    }
 }
--- app/src/main/java/com/example/appointmenttothedoctor/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/example/appointmenttothedoctor/ui/profile/ProfileViewModel.java
@@ -1,19 +1,46 @@
 package com.example.appointmenttothedoctor.ui.profile;
 
+import android.net.Uri;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ProfileViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<String> user;
+   // private final MutableLiveData<String> verified;
 
     public ProfileViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        user = new MutableLiveData<>();
+   //     verified = new MutableLiveData<>();
+      //  mText.setValue("This is notifications fragment");
+        getUserProfile();
     }
 
     public LiveData<String> getText() {
-        return mText;
+        return user;
+    }
+
+    public void getUserProfile() {
+        // [START get_user_profile]
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            // String name = user.getDisplayName();
+            String email = user.getEmail();
+
+            this.user.setValue("Пользователь: " + email);
+           // Uri photoUrl = user.getPhotoUrl();
+            boolean emailVerified = user.isEmailVerified();
+
+           // this.verified.setValue("zfdsfsdf");
+
+           // String uid = user.getUid();
+        }
+        // [END get_user_profile]
     }
 }
